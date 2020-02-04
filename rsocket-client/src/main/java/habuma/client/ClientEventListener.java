f6c3b56ed02b667258c59649e6c78887cc608cb4
Craig Walls <cwalls@pivotal.io>
2020-02-04 13:13:03 -0700

    Add multicast to route

--- rsocket-client/src/main/java/habuma/client/ClientEventListener.java
+++ rsocket-client/src/main/java/habuma/client/ClientEventListener.java
@@ -1,20 +1,28 @@
 package habuma.client;
 
-import org.springframework.context.ApplicationListener;
+import org.springframework.cloud.gateway.rsocket.client.BrokerClient;
 import org.springframework.context.PayloadApplicationEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public class ClientEventListener implements ApplicationListener<PayloadApplicationEvent<RSocketRequester>>{
+@RequiredArgsConstructor
+public class ClientEventListener {
 
-	@Override
-	public void onApplicationEvent(PayloadApplicationEvent<RSocketRequester> event) {
+	private final BrokerClient brokerClient;
+
+	@EventListener
+	public void getClient(PayloadApplicationEvent<RSocketRequester> event) {
 		event.getPayload()
 			.route("refreshing-service-rr")
+			.metadata(brokerClient.forwarding(builder -> 
+					builder.serviceName("refreshing-service")
+						.with("multicast", "true")))
 			.data("Refreshing Client Connection Request")
 			.retrieveFlux(RefreshEvent.class)
 			.doOnNext(c -> {
